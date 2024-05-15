5186c50aee13130964930672755545842cd13509
effiecancode <faitheffie25@gmail.com>
2024-05-15 23:51:54 +0300

    document db.js

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 // Importing the required module from 'mongodb' package
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 // Defining default connection parameters
-const host = process.env.DB_HOST || "localhost";
+const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
-const database = process.env.DB_DATABASE || "files_manager";
+const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}/`;
 
 // Class def for managing MongoDB connection and operations
@@ -16,8 +16,8 @@ class DBClient {
     MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
       if (error) console.log(error);
       this.db = client.db(database);
-      this.db.createCollection("users");
-      this.db.createCollection("files");
+      this.db.createCollection('users');
+      this.db.createCollection('files');
     });
   }
 
@@ -29,20 +29,20 @@ class DBClient {
 
   // Method to retrieve the number of documents in the 'users' collection
   async nbUsers() {
-    return this.db.collection("users").countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   // Method to retrieve a user from the 'users' collection based on a query
   async getUser(query) {
-    console.log("QUERY IN DB.JS", query);
-    const user = await this.db.collection("users").findOne(query);
-    console.log("GET USER IN DB.JS", user);
+    console.log('QUERY IN DB.JS', query);
+    const user = await this.db.collection('users').findOne(query);
+    console.log('GET USER IN DB.JS', user);
     return user; // Return the retrieved user
   }
 
   // Method to retrieve the number of documents in the 'files' collection
   async nbFiles() {
-    return this.db.collection("files").countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 }
 // Instantiate DBClient class
