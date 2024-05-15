62bbde75d79e3a44a60216790975c381908709d7
effiecancode <faitheffie25@gmail.com>
2024-05-15 22:34:48 +0300

    eslint fix

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
-const host = process.env.DB_HOST || "localhost";
+const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
-const database = process.env.DB_DATABASE || "files_manager";
+const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}/`;
 
 class DBClient {
@@ -11,8 +11,8 @@ class DBClient {
     MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
       if (error) console.log(error);
       this.db = client.db(database);
-      this.db.createCollection("users");
-      this.db.createCollection("files");
+      this.db.createCollection('users');
+      this.db.createCollection('files');
     });
   }
 
@@ -21,18 +21,18 @@ class DBClient {
   }
 
   async nbUsers() {
-    return this.db.collection("users").countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   async getUser(query) {
-    console.log("QUERY IN DB.JS", query);
-    const user = await this.db.collection("users").findOne(query);
-    console.log("GET USER IN DB.JS", user);
+    console.log('QUERY IN DB.JS', query);
+    const user = await this.db.collection('users').findOne(query);
+    console.log('GET USER IN DB.JS', user);
     return user;
   }
 
   async nbFiles() {
-    return this.db.collection("files").countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 }
 
